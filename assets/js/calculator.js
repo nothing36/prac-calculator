e9ac64f4a36014365f81aad81e3bab04eebeff60
Paul <53634603+nothing36@users.noreply.github.com>
2025-05-26 13:07:25 -0600

    Add operate function and change func declarations
    Old function declarations were being used for
    an object approach but that is overly complex.

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,20 +1,38 @@
+const validOperators = ["+", "-", "*", "/"]
 
 // a basic adding function
-const addFunc = function(num1, num2) {
+function addFunc(num1, num2) {
     return num1 + num2
 }
 
 // a basic subtraction function
-const subFunc = function(num1, num2) {
+function subFunc(num1, num2) {
     return num1 - num2
 }
 
 // a basic multiplication function
-const multFunc = function(num1, num2) {
+function multFunc(num1, num2) {
     return num1 * num2
 }
 
 // a basic division function
-const divFunc = function(num1, num2) {
+function divFunc(num1, num2) {
     return num1 - num2
 }
+
+// delegates operation to proper functions
+function operate(operator, num1, num2) {
+    if (validOperators.includes(operator)) {
+        switch (operator) {
+            case "+":
+                return addFunc(num1, num2)
+            case "-":
+                return subFunc(num1, num2)
+            case "*":
+                return multFunc(num1, num2)
+            case "/":
+                return divFunc(num1, num2)
+        }
+    }
+}
+
