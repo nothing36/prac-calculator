75c8a62d7627167b33e309f2d7916247f328ef92
Paul <53634603+nothing36@users.noreply.github.com>
2025-05-26 12:52:24 -0600

    Create basic math operation functions

---
+++ assets/js/calculator.js
@@ -0,0 +1,20 @@
+
+// a basic adding function
+const addFunc = function(num1, num2) {
+    return num1 + num2
+}
+
+// a basic subtraction function
+const subFunc = function(num1, num2) {
+    return num1 - num2
+}
+
+// a basic multiplication function
+const multFunc = function(num1, num2) {
+    return num1 * num2
+}
+
+// a basic division function
+const divFunc = function(num1, num2) {
+    return num1 - num2
+}
